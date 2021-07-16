825366b297ff273d5e8448c7bda19d193859d64c
irieda <irieda@gmail.com>
2021-07-16 11:33:59 +0900

    add gateway implement

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -12,11 +12,13 @@ void BoardGetUniqueId(uint8_t *id);
 void onRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
   if (size == 11) {
     Serial.print("RX:\t");
-    Serial.printf("rssi=%d\tsnr=%d\tid=", size, rssi, snr);
-    Serial.printBuffer(payload, 8);
+    Serial.printf("rssi=%d\tsnr=%d\t", rssi, snr);
+    Serial.printf("id=%02x%02x%02x%02x%02x%02x%02x%02x\t", payload[0],
+                  payload[1], payload[2], payload[3], payload[4], payload[5],
+                  payload[6], payload[7]);
     uint16_t v = *(uint16_t *)(&payload[8]);
-    Serial.printf("\tbattery=%4.2f", float(v) / 100.0);
-    Serial.printf("\tstate=%d\n", payload[10]);
+    Serial.printf("battery=%4.2f\t", float(v) / 100.0);
+    Serial.printf("state=%d\n", payload[10]);
   }
 }
 
