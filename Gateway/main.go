90eba158ebb0af0716a92a4f61edbefb90d19b50
Irieda Noboru <irieda@144lab.com>
2022-05-31 20:01:07 +0900

    change Gateway imprement

--- Gateway/main.go
+++ Gateway/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bufio"
 	"context"
-	"flag"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strconv"
@@ -12,13 +13,27 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/brutella/hc"
-	"github.com/brutella/hc/accessory"
-	hclog "github.com/brutella/hc/log"
-	"github.com/brutella/hc/service"
+	"github.com/caarlos0/env/v6"
 	"github.com/tarm/serial"
-	qrcode "github.com/yeqown/go-qrcode"
 )
+
+type Config struct {
+	Port        string        `env:"RECEIVER_PORT" envDefault:"/dev/ttyS0"`
+	ID          string        `env:"TRANSMITTER_ID, required"`
+	Token       string        `env:"LINE_NOTIFY_TOKEN, required"`
+	OnMessage   string        `env:"ON_MESSAGE" envDefault:"何か届きました"`
+	OffMessage  string        `env:"OFF_MESSAGE" envDefault:"何か届きました"`
+	WaitTime    time.Duration `env:"WAIT_TIME" envDefault:"10s"`
+	BatteryWarn float64       `env:"BATTERY_WARN" envDefault:"2.2"`
+}
+
+var cfg = &Config{}
+
+func init() {
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+}
 
 type State int
 
@@ -59,7 +74,6 @@ type Record struct {
 type gateway struct {
 	id   string
 	port *serial.Port
-	acc  *MailboxSensor
 }
 
 func (g *gateway) scanner(recv chan<- Record) {
@@ -113,7 +127,30 @@ func (g *gateway) scanner(recv chan<- Record) {
 	}
 }
 
+func (g *gateway) postMessage(ctx context.Context, msg string) {
+	values := url.Values{}
+	values.Set("message", msg)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://notify-api.line.me/api/notify", strings.NewReader(values.Encode()))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Bearer "+cfg.Token)
+	req.Header.Set("User-Agent", "Line Notify")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Print(resp.Status)
+	}
+}
+
 func (g *gateway) parser(ctx context.Context, recv <-chan Record) {
+	last := time.Now().Add(-cfg.WaitTime)
 	for {
 		select {
 		case <-ctx.Done():
@@ -122,16 +159,28 @@ func (g *gateway) parser(ctx context.Context, recv <-chan Record) {
 			if !ok {
 				return
 			}
-			log.Println(v)
-			if strings.EqualFold(v.ID, g.id) {
-				switch v.State {
-				case EMPTY:
-					g.acc.State.ContactSensorState.SetValue(0)
-				case DETECT:
-					g.acc.State.ContactSensorState.SetValue(1)
-				}
-				g.acc.Battery.BatteryLevel.SetValue(int(100 * v.Battery / 3.0))
+			if time.Since(last) < cfg.WaitTime {
+				log.Println(v, "->skip")
+				continue
 			}
+			log.Println(v)
+			if !strings.EqualFold(v.ID, cfg.ID) {
+				continue
+			}
+			msg := ""
+			switch v.State {
+			default:
+				continue
+			case EMPTY:
+				msg = cfg.OffMessage
+			case DETECT:
+				msg = cfg.OnMessage
+			}
+			if v.Battery < cfg.BatteryWarn {
+				msg += "\n(バッテリー残量が低いです)"
+			}
+			g.postMessage(ctx, msg)
+			last = time.Now()
 		}
 	}
 }
@@ -143,71 +192,17 @@ func (g *gateway) proc(ctx context.Context) {
 	g.parser(ctx, ch)
 }
 
-// MailboxSensor ...
-type MailboxSensor struct {
-	*accessory.Accessory
-	State   *service.ContactSensor
-	Battery *service.BatteryService
-}
-
-// NewMailbox returns a Door state
-func NewMailbox() *MailboxSensor {
-	info := accessory.Info{
-		Name: "Mailbox",
-	}
-	acc := &MailboxSensor{}
-	acc.Accessory = accessory.New(info, accessory.TypeSensor)
-	acc.State = service.NewContactSensor()
-	acc.State.ContactSensorState.SetValue(0)
-	acc.Battery = service.NewBatteryService()
-	acc.Battery.BatteryLevel.SetValue(100)
-	acc.AddService(acc.State.Service)
-	acc.AddService(acc.Battery.Service)
-	return acc
-}
-
 func main() {
-	var id string
-	flag.StringVar(&id, "id", "73c19db9f3555d09", "device id(16 digit hex)")
-	flag.Parse()
-	if flag.NArg() != 1 {
-		log.Fatal("need an argument what is name of serial-port")
-	}
-	hclog.Debug.Enable()
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGHUP, syscall.SIGTERM)
 	defer stop()
 
-	acc := NewMailbox()
-	t, err := hc.NewIPTransport(hc.Config{
-		Port: "12345", Pin: "23000214",
-	}, acc.Accessory)
-	if err != nil {
-		log.Fatal(err)
-	}
-	u, err := t.XHMURI()
-	if err != nil {
-		log.Fatal(err)
-	}
-	qrc, err := qrcode.New(u)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := qrc.Save("qrcode.png"); err != nil {
-		log.Fatal(err)
-	}
-	hc.OnTermination(func() {
-		t.Stop()
-	})
-	go t.Start()
-
-	port, err := serial.OpenPort(&serial.Config{Name: flag.Arg(0), Baud: 115200})
+	port, err := serial.OpenPort(&serial.Config{Name: cfg.Port, Baud: 115200})
 	if err != nil {
 		log.Fatal(err)
 	}
 	g := &gateway{
-		id:   id,
+		id:   cfg.ID,
 		port: port,
-		acc:  acc,
 	}
 	go g.proc(ctx)
 
